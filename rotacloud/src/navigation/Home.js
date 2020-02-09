50de0ec722a314b6d90656785e1029d70ddeb9f0
Anna Winther <anski.anna@gmail.com>
2020-02-09 21:06:12 +0000

    Edit comments and fix console warning

--- rotacloud/src/navigation/Home.js
+++ rotacloud/src/navigation/Home.js
@@ -3,7 +3,6 @@ import React from 'react'
 export default function Home() {
     return (
          <section id="home">
-
             <h1>The Challenge</h1>
 
             <p>
@@ -26,20 +25,17 @@ export default function Home() {
                 Please complete the challenge in a GitHub repo and invite 'david-brandon' as a collaborator upon completion. If you have any questions, please email <a href="mailto:dev@rotacloud.com">dev@rotacloud.com</a>.
             </p>
 
-
-
             <h2>Important Info</h2>
 
             <p>
                 The JSON data for the users is located at:<br />
-                <a href="https://custom.rotacloud.com/angular-challenge/users.json" target="_blank">https://custom.rotacloud.com/angular-challenge/users.json</a>
+                <a href="https://custom.rotacloud.com/angular-challenge/users.json" target="_blank" rel="noopener noreferrer">https://custom.rotacloud.com/angular-challenge/users.json</a>
             </p>
 
             <p>
                 The JSON data for the roles is located at:<br />
-                <a href="https://custom.rotacloud.com/angular-challenge/roles.json" target="_blank">https://custom.rotacloud.com/angular-challenge/roles.json</a>
+                <a href="https://custom.rotacloud.com/angular-challenge/roles.json" target="_blank" rel="noopener noreferrer">https://custom.rotacloud.com/angular-challenge/roles.json</a>
             </p>
-
-    </section>
+         </section>
     )
 }
