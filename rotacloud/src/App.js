feeab5670b2321aa142a77b7536832e56e4d3616
Anna Winther <anski.anna@gmail.com>
2020-02-09 07:44:10 +0000

    Sort roles and users

--- rotacloud/src/App.js
+++ rotacloud/src/App.js
@@ -36,14 +36,16 @@ class App extends React.Component {
         
         let filteredUsers = users.filter(user => { return user.roles !== null}); // remove users which do not have a role
 
-        for (let i = 0; i < filteredUsers.length; i++){
-          let namedRoles = filteredUsers[i].roles.map(role => {
+        for (let i = 0; i < filteredUsers.length; i++){ //lopps over the filteredUsers
+          let namedRoles = filteredUsers[i].roles.map(role => { // takes the role array inside of the filtered users and maps over it to get each role id of the user
               let roleIdFromUserAxios = role
               let userInfo = rolesAxios.find(roleFromRoles => roleFromRoles.id === roleIdFromUserAxios);
               let roleName = userInfo.name;
               return roleName
           })
           filteredUsers[i].roles = namedRoles;
+          let sortRoles = filteredUsers[i].roles;
+          sortRoles.sort();
         }
         this.setState({ users: users })
       }))
@@ -63,15 +65,34 @@ getRoles() {
 
       for (var i = 0; i < rolesAxios.length; i++) {
           var matchedUsers = filteredUsers.filter(users => users.roles.includes(rolesAxios[i].id)); // match users with role ID
-          rolesAxios[i].users = matchedUsers; 
+          rolesAxios[i].users = matchedUsers;
+
+          let sortUsers = rolesAxios[i].users;
+          sortUsers.sort(dynamicSort('name'));
+      }
+         function dynamicSort(property) {
+          var sortOrder = 1;
+          if(property[0] === "-") {
+              sortOrder = -1;
+              property = property.substr(1);
+          }
+          return function (a,b) {
+              /* next line works with strings and numbers, 
+               * and you may want to customize it to your needs
+               */
+              var result = (a[property] < b[property]) ? -1 : (a[property] > b[property]) ? 1 : 0;
+              return result * sortOrder;
+          }
       }
 
+    
       this.setState({roles: rolesAxios});
   }));
 }
 
 
   render() {
+    console.log('users', this.state.users)
     return (
       <div>
         <h1 onClick={this.findRolebyUser}>Hello</h1>
